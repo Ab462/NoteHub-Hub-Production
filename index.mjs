bd94756dd0aeed64f5189cf3f5a185ad3084f5ef
Abdul Wasay <wasay0010@gmail.com>
2023-11-11 16:38:49 +0500

    hello

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ import cors from "cors";
 import("./db.mjs");
 
 const app = express();
-const port = 8000 || process.env.PORT;
+const port = 8000;
 dotenv.config();
 
 // current directory
@@ -17,15 +17,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join("./Client/build")));
+// app.use(express.static(path.join("./Client/build")));
 
 // Available Routes
 app.use("/api/auth", auth);
 app.use("/api/notes", notes);
 
-app.get("*", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "./Client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.status(200).sendFile(path.join(__dirname, "./Client/build/index.html"));
+// });
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
