aab9a5a8a87a05900c6a95e16f795014539b7596
Abdul Wasay <wasay0010@gmail.com>
2023-11-11 15:15:11 +0500

    everything changed

--- index.mjs
+++ index.mjs
@@ -1,28 +1,32 @@
 import express from "express";
 import auth from "./routes/auth.mjs";
 import notes from "./routes/notes.mjs";
-import cors from "cors";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-import path from "path";
+import path,{ dirname } from "path";
+import cors from "cors";
 import("./db.mjs");
 
 const app = express();
-const port = 8000;
+const port = 8000 || process.env.PORT;
 dotenv.config();
+
+// current directory
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join('./Client/build')));
+app.use(express.static(path.join("./Client/build")));
 
 // Available Routes
 app.use("/api/auth", auth);
 app.use("/api/notes", notes);
 
-app.use("*",(req,res)=>{
-  res.status(200).sendFile('./Client/build/index.html');
-})
+app.get("*", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "./Client/build/index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Server is listening at https://localhost:${port}`);
+  console.log(`Server is listening at localhost:${port}`);
 });
