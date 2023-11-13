b738fc5ad31b1ee6b674e77c455753bc63dd43f9
Abdul Wasay <wasay0010@gmail.com>
2023-11-13 17:00:50 +0500

    hamo

--- index.mjs
+++ index.mjs
@@ -1,9 +1,7 @@
 import express from "express";
 import auth from "./routes/auth.mjs";
 import notes from "./routes/notes.mjs";
-import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-import path,{ dirname } from "path";
 import cors from "cors";
 import("./db.mjs");
 
@@ -11,22 +9,17 @@ const app = express();
 const port = 8000;
 dotenv.config();
 
-// current directory
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join("./Client/build")));
+app.use(express.static('./build'));
 
 // Available Routes
 app.use("/api/auth", auth);
 app.use("/api/notes", notes);
 
-app.get("*", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "./Client/build/index.html"));
-});
 
 app.listen(port, () => {
-  console.log(`Server is listening at https://localhost:${port}`);
+  console.log(`Server is listening at http://localhost:${port}`);
 });
